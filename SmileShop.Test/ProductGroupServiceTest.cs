6e091426311c16f17e531203a1e989507352da35
21aki <akiakira@live.com>
2021-02-10 11:20:04 +0700

    First Test Success

--- SmileShop.Test/ProductGroupServiceTest.cs
+++ SmileShop.Test/ProductGroupServiceTest.cs
@@ -9,59 +9,45 @@ using Microsoft.AspNetCore.Http;
 using Moq;
 using SmileShop.Services;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using SmileShop.DTOs;
 
 namespace SmileShop.Test
 {
     [TestClass]
-    public class ProductGroupServiceTest
+    public class ProductGroupServiceTest : TestBase
     {
 
-        /// <summary>
-        /// Create new dbContext for each test.
-        /// </summary>
-        /// <param name="dbName"> Name new db with uniqe name for uniqe testing.</param>
-        /// <returns>Project's DBContext</returns>
-        protected AppDBContext BuildContext(string dbName)
-        {
-            var options = new DbContextOptionsBuilder<AppDBContext>()
-                .UseInMemoryDatabase(dbName).Options;
-
-            var dbContext = new AppDBContext(options);
-            return dbContext;
-        }
-
-        /// <summary>
-        /// Create auto mapper from our project.
-        /// </summary>
-        /// <returns>AutoMapper</returns>
-        protected IMapper BuildMap()
-        {
-            var config = new MapperConfiguration(options =>
-            {
-                options.AddProfile(new AutoMapperProfile());
-            });
-
-            return config.CreateMapper();
-        }
         /// <summary>
         /// If Product Group method GetAll has no data in database
         /// it must return suscess with error message
         /// </summary>
         [TestMethod]
-        public async Task ProductGroupGetAllHasNoData()
+        public async Task ProductGroupGetAll_NoData_ReturnErrorMessage()
         {
+            // Arrange
             var dbName = Guid.NewGuid().ToString();
             var context = BuildContext(dbName);
             var mapper = BuildMap();
-            var httpContext = new Mock<IHttpContextAccessor>();
-            
-            var service = new ProductGroupServices(context, mapper, httpContext.Object);
 
-            var response = await service.GetAll(null, null, null);
 
-            var result = response.IsSuccess;
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var http = new DefaultHttpContext();
+            // var fakeTenantId = "abcd";
+            // context.Request.Headers["Tenant-ID"] = fakeTenantId;
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(http);
 
-            Assert.AreEqual(result, false);
+            var pagination = new PaginationDto();
+            var filter = "";
+            var order = new DataOrderDTO();
+
+            // Act
+            var service = new ProductGroupServices(context, mapper, mockHttpContextAccessor.Object);
+            var result = await service.GetAll(pagination, filter, order);
+
+            // Assert
+            Assert.AreEqual(result.IsSuccess, false);
+            Assert.AreEqual(result.Message, "No Product Group in this query");
         }
     }
 }
---
+++ SmileShop.Test/TestBase.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SmileShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmileShop.Test
+{
+    public class TestBase
+    {
+        /// <summary>
+        /// Create new dbContext for each test.
+        /// </summary>
+        /// <param name="dbName"> Name new db with uniqe name for uniqe testing.</param>
+        /// <returns>Project's DBContext</returns>
+        protected AppDBContext BuildContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<AppDBContext>()
+                .UseInMemoryDatabase(dbName).Options;
+
+            var dbContext = new AppDBContext(options);
+            return dbContext;
+        }
+
+        /// <summary>
+        /// Create auto mapper from our project.
+        /// </summary>
+        /// <returns>AutoMapper</returns>
+        protected IMapper BuildMap()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapperProfile());
+            });
+
+            return config.CreateMapper();
+        }
+
+    }
+}

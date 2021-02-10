1aac7838a07d726911c91799b745f738d471ee7a
21aki <akiakira@live.com>
2021-02-10 13:14:56 +0700

    + Mockup Data Method

--- SmileShop.Test/ProductGroupServiceTest.cs
+++ SmileShop.Test/ProductGroupServiceTest.cs
@@ -50,6 +50,46 @@ namespace SmileShop.Test
             Assert.AreEqual(result.Message, "No Product Group in this query");
         }
 
+        // GetAll_HaveData_ReturnResultWithPagination
+        // GetAll_CanDoPaginationOn2ndPage_ReturnResultWithPagination
+        // GetAll_CanFilterByName_ReturnFilteredListOfProductGroup
 
+        // GetList_NoData_ReturnErrorMessage
+        // GetList_NoFilter_ReturnErrorMessage
+        // GetList_HaveDataAndFilter_ReturnFilteredListOfProductGroup
+
+        // Get_NoData_ReturnErrorMessage
+        // Get_ProductIdIsLessOrEqualZero_ReturnErrorMessage
+        // Get_HaveDataButIdIsNotExist_ReturnErrorMessage
+        // Get_HaveDataAndProductID_ReturnProductGroupWithSameId
+
+        // Add_NoLoginUser_ReturnErrorMessage
+        // Add_SentBlankProductGroupName_ReturnErrorMessage
+        // Add_WithData_ReturnAddedResult
+
+        // Edit_NoData_ReturnErrorMessage
+        // Edit_ProductIdIsLessOrEqualZero_ReturnErrorMessage
+        // Edit_HaveDataButIdIsNotExist_ReturnErrorMessage
+        // Edit_SentBlankProductGroupName_ReturnErrorMessage
+        // Edit_WithData_ReturnEditedResult
+
+        // Delete_NoData_ReturnErrorMessage
+        // Delete_ProductIdIsLessOrEqualZero_ReturnErrorMessage
+        // Delete_HaveDataButIdIsNotExist_ReturnErrorMessage
+        // Delete_WithID_ReturnDeletedResult
+
+        public List<ProductGroup> ProductGroupData()
+        {
+            List<ProductGroup> productGroups = new List<ProductGroup> {
+                new ProductGroup { Id = 1, Name = "Test Product Group 1", CreatedByUserId = Guid.NewGuid(), CreatedDate = DateTime.Now },
+                new ProductGroup { Id = 2, Name = "Test Product Group 2", CreatedByUserId = Guid.NewGuid(), CreatedDate = DateTime.Now },
+                new ProductGroup { Id = 3, Name = "Test Product Group 3", CreatedByUserId = Guid.NewGuid(), CreatedDate = DateTime.Now },
+                new ProductGroup { Id = 4, Name = "Test Product Group 4", CreatedByUserId = Guid.NewGuid(), CreatedDate = DateTime.Now },
+                new ProductGroup { Id = 5, Name = "Test Product Group 5", CreatedByUserId = Guid.NewGuid(), CreatedDate = DateTime.Now },
+            };
+
+            return productGroups;
+        
+        }
     }
 }
